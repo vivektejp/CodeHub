63324a1eb6066103b08f630b756bda1e60113942
SharathChandraReddy-Nagireddy <126187548+SharathChandraReddy-Nagireddy@users.noreply.github.com>
2023-05-31 21:42:31 -0500

    day2 assignment

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello world");
+        Student s = new Student();
+        s.setName("vivek");
+        System.out.printf("Name of the student: "+ s.getName());
+    }
+}
